3f6e088d25ea0e327e9e441faf543dfb77dd7edb
dianamishanich <diana-mishanich@ya.ru>
2021-03-04 01:23:04 +0300

    ReqresInTests

--- src/test/java/model/User.java
+++ src/test/java/model/User.java
@@ -3,7 +3,6 @@ package model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
 
